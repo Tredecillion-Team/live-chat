02644c9f470e642a33bc5342121a1fd413380649
Tredecillion <agus_bragas@yahoo.co.id>
2025-05-24 13:38:16 +0700

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
+
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, '.', '');
+    return {
+      define: {
+        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+      },
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, '.'),
+        }
+      },
+      server: {
+        host: true, // Ini akan membuat server dapat diakses secara eksternal
+        allowedHosts: ['233420d24c3fbf2082f5b13ca5afbdf6.serveo.net']
+      }
+    };
+});
